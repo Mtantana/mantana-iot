51ff7c9bd1103ff6b68fd2e6e647acf4139e0ac9
Mtantana <88876266+Mtantana@users.noreply.github.com>
2021-08-18 22:23:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <body onload="JavaScript:timedRefresh(15000);"> 
-<br><h1>Wasinee Na Ranong  62108659 </h1></br>
+<br><h1>Mantana Laaiadkan  62107594 </h1></br>
 
-<iframe src="https://thingspeak.com/channels/1458415/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"></iframe>
-<iframe src="https://thingspeak.com/channels/1458415/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"></iframe>
-<iframe src="https://thingspeak.com/channels/1458415/maps/channel_show" width="450" height="260"></iframe>
+<iframe src="https://thingspeak.com/channels/1458409/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"></iframe>
+<iframe src="https://thingspeak.com/channels/1458409/charts/2?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15" width="450" height="260"></iframe>
+<iframe src="https://thingspeak.com/channels/1458409/maps/channel_show" width="450" height="260"></iframe>
 <?php
- $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458415/fields/1/last.txt');
- $Humidity = file_get_contents('https://api.thingspeak.com/channels/1458415/fields/2/last.txt');
+ $Temperature = file_get_contents('https://api.thingspeak.com/channels/1458409/fields/1/last.txt');
+ $Humidity = file_get_contents('https://api.thingspeak.com/channels/1458409/fields/2/last.txt');
 ?>
 <script type="text/JavaScript">
 function timedRefresh(timeoutPeriod) {
