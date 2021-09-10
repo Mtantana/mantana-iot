a5f6a10dbce5a1380157aa73803f363f1e69a4bf
Mtantana <mantana.la@mail.wu.ac.th>
2021-09-11 00:31:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     
-    <title>Project 62107594  Mantana Laaiadkan</title>
+    <title>Show Graph</title>
     
     <script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
@@ -15,7 +15,7 @@
   <body>
 
     <div class="container">
-        <p><b>Show Graph</b></p>
+        <p><b>62107594  Mantana Laaidakan</b></p>
     </div>
 
     <div class="container">
@@ -124,7 +124,7 @@
 
         $(()=>{
           
-          let url = " https://api.thingspeak.com/channels/1502769/feeds.json?results=2";
+          let url = " https://api.thingspeak.com/channels/1479261/feeds.json?results=240";
 
           $.getJSON(url)
             .done(function(data){
